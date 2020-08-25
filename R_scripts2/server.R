4b79b3cd2e21a75cdaa812625c7d331b1cbfea5f
jrobledob <jrobledob@hotmail.com>
2020-08-25 07:00:16 -0500

    trial with check boxes

--- R_scripts2/server.R
+++ R_scripts2/server.R
@@ -8,8 +8,18 @@ shinyServer(
     output$table <- renderTable({
       criterio <- input$Criterio
       primera_busqueda <- input$primera_busqueda
+      primera_busqueda2 <- input$primera_busqueda2
+      primera_busqueda3 <- input$primera_busqueda3
+      primera_busqueda4 <- input$primera_busqueda4
+      primera_busqueda5 <- input$primera_busqueda5
+      primera_busqueda6 <- input$primera_busqueda6
       print(criterio)
       print(primera_busqueda)
+      print(primera_busqueda2)
+      print(primera_busqueda3)
+      print(primera_busqueda4)
+      print(primera_busqueda5)
+      print(primera_busqueda6)
       data[which(apply(data, 1, function(r) any(r %in% c(primera_busqueda)))),]
     }
     )
--- R_scripts2/ui.R
+++ R_scripts2/ui.R
@@ -13,24 +13,34 @@ shinyUI(
   pageWithSidebar(
     headerPanel("Record APP"),
     sidebarPanel(
-      selectizeInput("Criterio", "Seleccione el criterio principal de 
-                  búsqueda", choices = c("Producto", "Ingrediente activo","Uso agricola", 
-                                        "Cultivo", "Nombre común de la peste", "Nombre científico de la peste")),
-      conditionalPanel(condition = "input.Criterio == 'Ingrediente activo'",
-                       selectizeInput("primera_busqueda", "seleccione el ingrediente activo",choices= IA)),
-      conditionalPanel(condition = "input.Criterio == 'Producto'",
-                       selectizeInput("primera_busqueda", "seleccione el producto",choices= productos)),
-      conditionalPanel(condition = "input.Criterio == 'Uso agricola'",
-                       selectizeInput("primera_busqueda", "seleccione el uso",choices= uso)),
-      conditionalPanel(condition = "input.Criterio == 'Cultivo'",
-                       selectizeInput("primera_busqueda", "seleccione el cultivo",choices= cultivo)),
-      conditionalPanel(condition = "input.Criterio == 'Nombre común de la peste'",
-                       selectizeInput("primera_busqueda", "seleccione el nombre común de la peste",choices= comun)),
-      conditionalPanel(condition = "input.Criterio == 'Nombre científico de la peste'",
-                       selectizeInput("primera_busqueda", "seleccione el nombre cientifico de la peste",choices= cientifico)),
+      checkboxInput("Producto", "Producto", FALSE),
+      checkboxInput("IA", "Ingrediente Activo", FALSE),
+      checkboxInput("Uso", "Uso Agrícola", FALSE),
+      checkboxInput("Cultivo", "Cultivo", FALSE),
+      checkboxInput("Ncomun", "Nombre común", FALSE),
+      checkboxInput("Ncientifico ", "Nombre científico", FALSE),
       
-      conditionalPanel(condition = "input.Distribution == 'Exponential'",
-                       textInput("Lamda", "Please select Exponential Lamda", 1))),
+     # selectInput("Criterio", "Seleccione el criterio principal de 
+     #            búsqueda", choices = c("Producto", "Ingrediente activo","Uso agricola", 
+     #                                    "Cultivo", "Nombre común de la peste", "Nombre científico de la peste")),
+      conditionalPanel(condition = "input.Producto == 1",
+                       selectInput("producto", "seleccione el producto",choices= productos, selected = NA)),
+     
+     
+      # conditionalPanel(condition = "input.Criterio == 'Producto'",
+      #                  selectInput("primera_busqueda2", "seleccione el producto",choices= productos,selected = NA)),
+      # conditionalPanel(condition = "input.Criterio == 'Uso agricola'",
+      #                  selectInput("primera_busqueda3", "seleccione el uso",choices= uso, selected = NA)),
+      # conditionalPanel(condition = "input.Criterio == 'Cultivo'",
+      #                  selectInput("primera_busqueda4", "seleccione el cultivo",choices= cultivo, selected = NA)),
+      # conditionalPanel(condition = "input.Criterio == 'Nombre común de la peste'",
+      #                  selectInput("primera_busqueda5", "seleccione el nombre común de la peste",choices= comun, selected = NULL)),
+      # conditionalPanel(condition = "input.Criterio == 'Nombre científico de la peste'",
+      #                  selectInput("primera_busqueda6", "seleccione el nombre cientifico de la peste",choices= cientifico, selected = NA)),
+      # 
+      # conditionalPanel(condition = "input.Distribution == 'Exponential'",
+      #                  textInput("Lamda", "Please select Exponential Lamda", 1))
+     ),
     mainPanel(
       tableOutput("table")
     )
